ae9243ac7ed2842ad79cde33bf9c6f2156f29d11
Andrew Smith <andrew@friarswood.net>
2021-01-26 09:20:03 +0000

    version 2.1.8

--- src/NDArrayUtils.h
+++ src/NDArrayUtils.h
@@ -5,6 +5,7 @@
 
 #include <vector>
 #include <numeric>
+#include <limits>
 #include <cassert>
 #include <iostream>
 #include <cmath>
@@ -219,7 +220,7 @@ std::vector<std::vector<int>> listify(const size_t pop, const NDArray<T>& t, int
       const std::vector<int64_t>& ref = index;
       for (size_t j = 0; j < t.dim(); ++j)
       {
-        list[j][pindex] = offset + ref[/*t.dim() - 1 - */j];
+        list[j][pindex] = offset + static_cast<int>(ref[/*t.dim() - 1 - */j]);
       }
       ++pindex;
     }
